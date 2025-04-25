a180480a916f4b5f6c5c56b750a99bbfc3605331
Yuning Gu <yuning.gu@outlook.com>
2025-04-26 00:02:08 +0800

    Fix: Use standard img tag for navbar avatar

--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
@@ -32,11 +31,10 @@ export default function NavigationBar({ lang }: { lang: string }) {
           {/* Logo 和用户信息 */}
           <div className="flex items-center space-x-4">
             <div className="relative w-10 h-10 rounded-full overflow-hidden">
-              <Image
-                src="/images/avatar.jpg"
+              <img
+                src="/site/images/avatar.jpg"
                 alt="Yuning Gu"
-                fill
-                className="object-cover"
+                className="object-cover w-full h-full"
               />
             </div>
             <div className="text-left">
